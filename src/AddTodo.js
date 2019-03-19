72ded449b4b21d3edb36e4f6a7f562111ff6f336
Alex Albright <alexanderjalbright@gmail.com>
2019-03-19 19:06:46 -0400

    Adding todo complete

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 
 export default class AddTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: ""
+    };
+  }
+  inputChange(event) {
+    this.setState({ todo: event.target.value });
+  }
   render() {
-    const { add } = this.props;
+    const add = this.props.add;
     return (
       <div>
         <label>
           Item:
-          <input type="text" onChange={add.bind(this)} />
+          <input
+            type="text"
+            value={this.state.todo}
+            onChange={this.inputChange.bind(this)}
+          />
         </label>
-        <button type="submit">Add</button>
+        <button onClick={add.bind(this, this.state.todo)}>Add</button>
       </div>
     );
   }
