ab18602728943f2f91263189e500fb9f5bb5762e
Alex Albright <alexanderjalbright@gmail.com>
2019-03-19 16:38:18 -0400

    in progress, created state

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 export default class AddTodo extends Component {
   render() {
+    const { addTodo } = this.props;
     return (
       <div>
         <label>
           Item:
-          <input type="text" />
+          <input type="text" onChange={addTodo} />
         </label>
         <button>Add</button>
       </div>
--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import AddTodo from "./AddTodo";
 import TodoList from "./TodoList";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoList: []
+    };
+  }
+
+  addTodo = x => {
+    const list = todoList;
+    list.push(x);
+    this.setState({ todoList: list });
+  };
+
   render() {
     return (
       <div>
-        <AddTodo />
+        <AddTodo todoList={this.addTodo} />
         <TodoList />
         <button>Reset All</button>
       </div>
